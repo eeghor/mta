f3450fd5d5b85e38675928fbf1bae80b5915f7c4
Unknown <eeghor@gmail.com>
2018-11-21 16:43:18 +1100

    updated package version

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from setuptools import setup
 import os
 
 setup(name='mta',
-      version='0.0.2',
+      version='0.0.3',
       description='Multi-Touch Attrobution',
       classifiers=[
       	'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.6'
       ],
       url='https://github.com/eeghor/mta',
       author='Igor Korostil',
       author_email='eeghor@gmail.com',
       license='MIT',
       packages=['mta'],
-      install_requires=['unidecode'],
       python_requires='>=3.6',
-      package_data={'mta': ['data/*.csv']},
+      package_data={'mta': ['data/*.csv.gz']},
       keywords='attribution marketing')
