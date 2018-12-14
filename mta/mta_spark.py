b277647f3e1ba4a9cf159caf371205ab1f304845
Unknown <eeghor@gmail.com>
2018-12-14 14:23:14 +1100

    first touch working

--- mta/mta_spark.py
+++ mta/mta_spark.py
@@ -6,6 +6,68 @@ import re
 import os
 import json
 import builtins
+from collections import defaultdict
+
+
+def normalize_dict(d):
+	"""
+	returns a value-normalized version of dictionary d
+	"""
+	sum_all_values = builtins.sum(d.values())
+
+	for _ in d:
+		d[_] = builtins.round(d[_]/sum_all_values, 6)
+
+	return d
+
+def remove_loops_path(s):
+
+	"""
+	input s is a path as a list of channels
+	remove transitions from a channel directly to itself, e.g. a > a
+	"""
+	
+	clean_path = []
+
+	for i, c in enumerate(s, 1):
+
+		if i == 1:
+			clean_path.append(c)
+		else:
+			if c != clean_path[-1]:
+				clean_path.append(c)
+
+
+	return clean_path
+
+remove_loops_path_UDF = udf(remove_loops_path, ArrayType(StringType()))
+
+def remove_loops(df):
+
+	df = df.withColumn('path', split(df.path, r'\s*>\s*'))
+
+	df.path = remove_loops_path_UDF(df.path)
+
+	# spit path into a list
+	df = df.withColumn('path', concat_ws(' > ', df.path))
+
+	# since now we may have some duplicate paths, we need to remove duplicates and update conversion counts
+	df = df.groupBy('path').sum().toDF(*['path', 'total_conversions', 'total_conversion_value', 'total_null'])
+
+	return df
+
+def first_touch(df):
+
+	first_touch = defaultdict(int)
+
+	df = remove_loops(df)
+	df = df.withColumn('path', split(df.path, r'\s*>\s*'))
+	df = df.withColumn('ch_1', df.path.getItem(0))
+
+	for row in df.select('ch_1', 'total_conversions').groupBy('ch_1').sum().toDF('channel', 'counts').collect():
+		first_touch[row['channel']] = row['counts']
+
+	return normalize_dict(first_touch)
 
 # in pyspark Spark session is readily available as spark
 spark = SparkSession.builder.master("local").appName("test session").getOrCreate()
@@ -18,7 +80,4 @@ df = spark.read.option("inferSchema", "true") \
 		.option("header", "true") \
 		.csv("data/data.csv.gz")
 
-# spit path into a list
-df = df.withColumn('path', split(df.path, r'\s*>\s*'))
-
-df.show(n=3)
+print(first_touch(df))
