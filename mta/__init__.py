766c9161f1ef62e52a559445c73a09850ea0a865
eeghor <eeghor@gmail.com>
2026-06-18 15:05:05 +0700

    version issue fixed

--- mta/__init__.py
+++ mta/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .mta import MTA, MTAConfig
 
-__all__ = ["MTA", "MTAConfig"]
+try:
+    __version__ = version("mta")
+except PackageNotFoundError:
+    # Package is not installed (e.g. running from source tree)
+    __version__ = "0.0.0"
+
+__all__ = ["MTA", "MTAConfig", "__version__"]
